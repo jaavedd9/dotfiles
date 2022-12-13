a4740d88e4fd240053ecc842cac1bd3b858a4b7a
Jaaved Khan <jaaved9.khan@gmail.com>
2022-12-13 14:35:55 +0300

    emacs helm: refactored and commented unused packages to fix find file not showing candidates issue

--- emacs.d/jak/init-helm.el
+++ emacs.d/jak/init-helm.el
@@ -1,8 +1,3 @@
-(setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
-(setq helm-recentf-fuzzy-match t)
-(setq helm-mode-fuzzy-match t)
-(setq helm-completion-in-region-fuzzy-match t)
-
 
 (use-package helm
   :ensure t
@@ -17,7 +12,15 @@
         helm-recentf-fuzzy-match    t
         helm-full-frame nil;; to select candidates in the same window
         helm-split-window-default-side 'same
+        helm-M-x-fuzzy-match t ;; optional fuzzy matching for helm-M-x
+        helm-recentf-fuzzy-match t
+        helm-mode-fuzzy-match t
+        helm-completion-in-region-fuzzy-match t
         )
+  (global-set-key (kbd "C-x C-i") 'helm-resume)
+  ;;(global-set-key (kbd "C-c C-i") 'helm-multi-swoop)
+  ;;(global-set-key (kbd "C-x C-i") 'helm-multi-swoop-all)
+  (global-set-key (kbd "C-c C-b") 'helm-bookmarks)
   (require 'helm-config)
   (helm-mode 1)
   ;;  (setq helm-split-window-inside-p t
@@ -49,40 +52,40 @@
   )
 
 
-(use-package helm-ag
-  :ensure t
-  )
+;; (use-package helm-ag
+  ;; :ensure t
+  ;; )
 
-(use-package helm-xref
-  :ensure t
-  )
+;; (use-package helm-xref
+;;   :ensure t
+;;   )
 
 ;;  Flx-based fuzzy intelligent sorting for helm
-(use-package helm-flx
-  :ensure t
-  :config
-  (helm-flx-mode +1)
-  (setq
-      helm-flx-for-helm-find-files t ;; t by default
-      helm-flx-for-helm-locate t) ;; nil by default
-  )
+;; (use-package helm-flx
+;;   :ensure t
+;;   :config
+;;   (helm-flx-mode +1)
+;;   (setq
+;;       helm-flx-for-helm-find-files t ;; t by default
+;;       helm-flx-for-helm-locate t) ;; nil by default
+;;   )
 
-(use-package helm-fuzzier
-  :ensure t 
-  :config
-  (helm-fuzzier-mode 1)
-  )
+;; (use-package helm-fuzzier
+;;   :ensure t 
+;;   :config
+;;   (helm-fuzzier-mode 1)
+;;   )
   
-(use-package fussy
-  :ensure t
-  :config
-  (push 'fussy completion-styles)
-  (setq
-   ;; For example, project-find-file uses 'project-files which uses
-   ;; substring completion by default. Set to nil to make sure it's using
-   ;; flx.
-   completion-category-defaults nil
-   completion-category-overrides nil))
+;; (use-package fussy
+;;   :ensure t
+;;   :config
+;;   (push 'fussy completion-styles)
+;;   (setq
+;;    ;; For example, project-find-file uses 'project-files which uses
+;;    ;; substring completion by default. Set to nil to make sure it's using
+;;    ;; flx.
+;;    completion-category-defaults nil
+;;    completion-category-overrides nil))
 
 (setq helm-completion-style 'emacs)
 
@@ -114,13 +117,13 @@
 ;;     ))
 
 
-(use-package helm-descbinds
-  :init
-  (helm-descbinds-mode 1)
-  :bind
-  (("C-c h h" . helm-descbinds))
-  :config
-  (setq helm-descbinds-window-style 'split-window))
+;; (use-package helm-descbinds
+;;   :init
+;;   (helm-descbinds-mode 1)
+;;   :bind
+;;   (("C-c h h" . helm-descbinds))
+;;   :config
+;;   (setq helm-descbinds-window-style 'split-window))
 
 
 (use-package helm-swoop
@@ -135,41 +138,27 @@
         helm-swoop-use-line-number-face t
         ;; helm-multi-swoop-edit-save t
         )
+    ;; When doing isearch, hand the word over to helm-swoop
+    (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+    ;; From helm-swoop to helm-multi-swoop-all
+    (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+    ;; When doing evil-search, hand the word over to helm-swoop
+    ;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
+
+    ;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
+    (define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
+
+    ;; Move up and down like isearch
+    (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
+    (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
+    (define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
+    (define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
   )
 
-;;(global-unset-key (kbd "C-i"))
-;;(global-unset-key (kbd "C-I"))
-
-;; Change the keybinds to whatever you like :)
-(global-set-key (kbd "C-c C-i") 'helm-swoop)
-;;(global-set-key (kbd "C-I") 'helm-swoop-back-to-last-point)
-(global-set-key (kbd "C-x C-i") 'helm-resume)
-;;(global-set-key (kbd "C-c C-i") 'helm-multi-swoop)
-;;(global-set-key (kbd "C-x C-i") 'helm-multi-swoop-all)
-(global-set-key (kbd "C-c C-b") 'helm-bookmarks)
 
 
-;;
-(global-set-key (kbd "C-c C-/") 'helm-find)
-
-;;(global-set-key (kbd "C-x C-c C-b") 'helm-resume)
 (global-unset-key (kbd "C-x c b"))
 
-;; When doing isearch, hand the word over to helm-swoop
-(define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
-;; From helm-swoop to helm-multi-swoop-all
-(define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
-;; When doing evil-search, hand the word over to helm-swoop
-;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
-
-;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
-(define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
-
-;; Move up and down like isearch
-(define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
-(define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
-(define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
-(define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
 
 
 (helm-adaptive-mode 1)
@@ -203,55 +192,55 @@
 ;; ;; spacemacs adds this advice to all sync helm sources.  I don't really understand its purpose, but it does make a difference.  Without this, fuzzy matching works differently.
 ;; (advice-add 'helm-make-source :around #'spacemacs//helm-make-source)
 
-(use-package helm-tramp
-  :ensure t
-  :config
-  (setq tramp-default-method "ssh")
-  (define-key global-map (kbd "C-c t") 'helm-tramp)
-  )
+;; (use-package helm-tramp
+;;   :ensure t
+;;   :config
+;;   (setq tramp-default-method "ssh")
+;;   (define-key global-map (kbd "C-c t") 'helm-tramp)
+;;   )
 
 (require 'helm-rg)
 
-;; (use-package helm-rg
-;;   :custom
-;;   (helm-rg--extra-args '("--max-columns" "400"))
-;;   (fk/helm-rg-fuzzy-match t)  ; I may wanna disable helm-rg's transform functionality
-;;   :custom-face
-;;   (helm-rg-file-match-face ((t (:inherit font-lock-type-face :weight bold :underline nil))))
-;;   (helm-rg-line-number-match-face ((t (:inherit line-number))))
-;;   :bind
-;;   ("C-M-s" . fk/helm-rg-dwim)
-;;   :config
-;;   (defun fk/helm-rg-dwim (&optional query)
-;;     "Smarter version of helm-rg.
-;; - Search in project if in a project else search in default (current) directory.
-;; - Start search with selected text if region is active or empty string.
-;; - Escape special characters when searching with selected text."
-;;     (interactive)
-;;     (let ((helm-rg-default-directory (or (projectile-project-root) default-directory))
-;;           (query (or query (fk/convert-string-to-rg-compatible (or (fk/get-selected-text) "")))))
-;;       (cl-letf (((symbol-function 'helm-rg--get-thing-at-pt) (lambda () query)))
-;;         (if fk/helm-rg-fuzzy-match
-;;             (call-interactively 'helm-rg)
-;;           (cl-letf (((symbol-function 'helm-rg--helm-pattern-to-ripgrep-regexp) (lambda (_) _)))
-;;             (call-interactively 'helm-rg)))
-;;         )))
+(use-package helm-rg
+  :custom
+  (helm-rg--extra-args '("--max-columns" "400"))
+  (fk/helm-rg-fuzzy-match t)  ; I may wanna disable helm-rg's transform functionality
+  :custom-face
+  (helm-rg-file-match-face ((t (:inherit font-lock-type-face :weight bold :underline nil))))
+  (helm-rg-line-number-match-face ((t (:inherit line-number))))
+  :bind
+  ("C-M-s" . fk/helm-rg-dwim)
+  :config
+  (defun fk/helm-rg-dwim (&optional query)
+    "Smarter version of helm-rg.
+- Search in project if in a project else search in default (current) directory.
+- Start search with selected text if region is active or empty string.
+- Escape special characters when searching with selected text."
+    (interactive)
+    (let ((helm-rg-default-directory (or (projectile-project-root) default-directory))
+          (query (or query (fk/convert-string-to-rg-compatible (or (fk/get-selected-text) "")))))
+      (cl-letf (((symbol-function 'helm-rg--get-thing-at-pt) (lambda () query)))
+        (if fk/helm-rg-fuzzy-match
+            (call-interactively 'helm-rg)
+          (cl-letf (((symbol-function 'helm-rg--helm-pattern-to-ripgrep-regexp) (lambda (_) _)))
+            (call-interactively 'helm-rg)))
+        )))
 
-;;   ;; Use a simpler header in the helm buffer.
-;;   (fset 'helm-rg--header-name (lambda (_) (concat "Search at " helm-rg--current-dir)))
+  ;; Use a simpler header in the helm buffer.
+  (fset 'helm-rg--header-name (lambda (_) (concat "Search at " helm-rg--current-dir)))
 
-;;   (defun fk/helm-rg-dwim-with-glob (glob &optional query)
-;;     (interactive)
-;;     (let ((helm-rg-default-glob-string glob))
-;;       (fk/helm-rg-dwim query))))
+  (defun fk/helm-rg-dwim-with-glob (glob &optional query)
+    (interactive)
+    (let ((helm-rg-default-glob-string glob))
+      (fk/helm-rg-dwim query))))
 
 
 (use-package helm-icons
-  :straight (:host github :repo "yyoncho/helm-icons")
-  :after helm
-  :config
-  (treemacs-resize-icons fk/default-icon-size)
-  (helm-icons-enable))
+   :straight (:host github :repo "yyoncho/helm-icons")
+   :after helm
+   :config
+   (treemacs-resize-icons fk/default-icon-size)
+   (helm-icons-enable))
 
 
 (setq helm-grep-ag-command (concat "rg"
@@ -291,8 +280,8 @@ With WITH-TYPES, ask for file types to search in."
   (mu-helm-rg default-directory with-types))
 
 
-(use-package helm-ls-git
-  :ensure t)
+;; (use-package helm-ls-git
+;;   :ensure t)
 
 (defun mu--project-root ()
   "Return the project root directory or `helm-current-directory'."
@@ -320,23 +309,23 @@ With WITH-TYPES, ask for file types to search in."
 ;;   ;; progress")
 ;;   (remove-hook 'delete-frame-functions 'helm--delete-frame-function))
 
-(use-package helm-c-yasnippet
-  :ensure t
-  )
+;; (use-package helm-c-yasnippet
+;;   :ensure t
+;;   )
 
 
-(use-package 
-  helm-shell-history 
-  :ensure t 
-  :config (add-hook 'eshell-mode-hook (lambda () 
-                                        (eshell-cmpl-initialize) 
-                                        (define-key eshell-mode-map [remap eshell-pcomplete]
-                                                    'helm-esh-pcomplete) 
-                                        (define-key eshell-mode-map (kbd "M-p")
-                                                    'helm-eshell-history))))
+;; (use-package 
+;;   helm-shell-history 
+;;   :ensure t 
+;;   :config (add-hook 'eshell-mode-hook (lambda () 
+;;                                         (eshell-cmpl-initialize) 
+;;                                         (define-key eshell-mode-map [remap eshell-pcomplete]
+;;                                                     'helm-esh-pcomplete) 
+;;                                         (define-key eshell-mode-map (kbd "M-p")
+;;                                                     'helm-eshell-history))))
 
 
-(use-package helm-systemd
-  :ensure t)
+;; (use-package helm-systemd
+;;   :ensure t)
 
 (provide 'init-helm)
